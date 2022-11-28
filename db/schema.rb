37e33c0b33314f34b0d3b7c687bf7b1e4fb9918a
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-28 23:45:58 +0300

    add the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_203405) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_204209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
+  create_table "grettings", force: :cascade do |t|
     t.text "messages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
